6dda0837c7fac1d51746c90f4a42c8167dcf6d25
KelvinQiu <kelvinqiu802@outlook.com>
2022-07-30 22:19:38 +0800

    feat: display event

--- src/components/EventBar.jsx
+++ src/components/EventBar.jsx
@@ -1,5 +1,19 @@
-const EventBar = () => {
-  return <div className='event-bar'></div>;
+import AddButton from './AddButton';
+
+const EventBar = ({ events, setEvents }) => {
+  return (
+    <div className='event-bar'>
+      <h1 className='event-bar-title'>.todo</h1>
+      <div className='event-container'>
+        <AddButton />
+        {events.map((item) => (
+          <div key={item.title} className='event over-hide'>
+            {item.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default EventBar;
