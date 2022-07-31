de7df1c5bfd7e8be07d17579f65b3c7ac5d01ace
KelvinQiu <kelvinqiu802@outlook.com>
2022-07-31 13:46:58 +0800

    fix: add event

--- src/components/EventBar.jsx
+++ src/components/EventBar.jsx
@@ -3,7 +3,7 @@ import AddEventButton from './AddEventButton';
 const EventBar = ({ events, setEvents, currentEvent, setCurrentEvent }) => {
   const handleAdd = () => {
     const title = prompt('Enter the Title:');
-    if (title) setEvents((prev) => [...prev, { title: title, tasks: {} }]);
+    if (title) setEvents((prev) => [...prev, { title: title, tasks: [] }]);
   };
 
   return (
