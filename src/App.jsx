0e5969f094f5b030dc3a19f086f897e75db66420
KelvinQiu <kelvinqiu802@outlook.com>
2022-08-01 12:00:19 +0800

    fix: remove mock data

--- src/App.jsx
+++ src/App.jsx
@@ -19,65 +19,6 @@ function App() {
         ];
   });
 
-  // const [events, setEvents] = React.useState([
-  //   {
-  //     title: 'Battleship',
-  //     ['To do']: [
-  //       {
-  //         name: 'Welcome Page',
-  //         id: uuid(),
-  //         details: 'Create welcome page and a start button',
-  //       },
-  //       {
-  //         name: 'Design Result Page',
-  //         id: uuid(),
-  //         details: 'Design the Result page and send it to Lydia.',
-  //       },
-  //     ],
-  //     ['In progress']: [
-  //       {
-  //         name: 'Board',
-  //         id: uuid(),
-  //         details: 'Beautifying the Board UI',
-  //       },
-  //     ],
-  //     ['Completed']: [
-  //       {
-  //         name: 'Discussion',
-  //         id: uuid(),
-  //         details: '8pm, Jul 31, Zomm Meeting with group partners',
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Reading',
-  //     ['To do']: [
-  //       {
-  //         name: 'Something',
-  //         id: uuid(),
-  //         details: 'Something Something Something',
-  //         state: 'To do',
-  //       },
-  //     ],
-  //     ['In progress']: [
-  //       {
-  //         name: 'Something',
-  //         id: uuid(),
-  //         details: 'SomethingSomethingSomething',
-  //         state: 'In progress',
-  //       },
-  //     ],
-  //     ['Completed']: [
-  //       {
-  //         name: 'Something',
-  //         id: uuid(),
-  //         details: 'SomethingSomethingSomething',
-  //         state: 'Completed',
-  //       },
-  //     ],
-  //   },
-  // ]);
-
   const [currentEvent, setCurrentEvent] = React.useState(events[0]);
 
   React.useEffect(() => {
